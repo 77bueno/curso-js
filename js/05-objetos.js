b0e6f4246d750194daa94c46134412e6540b0c5f
victor <124385408+77bueno@users.noreply.github.com>
2023-09-02 11:34:24 -0300

    Objetos

---
+++ js/05-objetos.js
@@ -0,0 +1,9 @@
+let livro = {
+    titulo: "Harry Potter",
+    ano: 1997,
+    volumes: 8,
+    autor: "J. K. Rowling"
+};
+
+// Meu livro preferido é: Harry Potter
+console.log("Nome livro preferido é: "+livro.titulo);
