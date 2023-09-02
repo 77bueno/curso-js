e7a89e85d713e9a8f4983257d3a705df606c1354
victor <124385408+77bueno@users.noreply.github.com>
2023-09-02 11:59:53 -0300

    Acessando esses dados

--- js/05-objetos.js
+++ js/05-objetos.js
@@ -15,9 +15,18 @@ let cliente = {
     nome: "Frederico",
     apelido: "Kiko",
     idade: 22,
-    telefones: [11-2233-4455, 11-94002-8922],
+    telefones: ["11-2233-4455","11-94002-8922"],
     medidas: {
         peso: 75,
-        altura: 1.70
+        altura: 1.79
     }
 };
+
+console.log( cliente.nome );
+console.log( cliente.idade + " anos." );
+
+// Acessando uma propriedade que também é array
+console.log(cliente.telefones[1]);
+
+// Acessando uma propriedade que também é um objeto
+console.log( cliente.medidas.altura );
