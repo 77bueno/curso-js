5c2061c2333ca935582f7dce4d2bbfc503013fba
victor <124385408+77bueno@users.noreply.github.com>
2023-09-02 11:50:42 -0300

    objeto contendo array e objeto

--- js/05-objetos.js
+++ js/05-objetos.js
@@ -6,4 +6,18 @@ let livro = {
 };
 
 // Meu livro preferido é: Harry Potter
-console.log("Nome livro preferido é: "+livro.titulo);
+console.log("Meu livro preferido é: "+livro.titulo);
+console.log(`O ano é ${livro.ano}.`);
+
+
+// Exemplo 2: objeto contendo array e outro objeto
+let cliente = {
+    nome: "Frederico",
+    apelido: "Kiko",
+    idade: 22,
+    telefones: [11-2233-4455, 11-94002-8922],
+    medidas: {
+        peso: 75,
+        altura: 1.70
+    }
+};
