eda3af1dcd65ea63deb7da3dc6e3fc2193ca4a5a
victor <124385408+77bueno@users.noreply.github.com>
2023-09-02 10:38:11 -0300

    Mini exercício de array

---
+++ js/04-arrays.js
@@ -0,0 +1,21 @@
+let alunos = ["Victor", "Phelipe", "Jhonatan", "Eliel"];
+
+/* O acesso aos dados individuais do array, é feito através do nome do array e do ÍNDICE correspondente à posição do dado. */
+console.log(alunos[2]);
+console.log(alunos[0]);
+
+// Mini-Exercício 
+
+let artistas = [
+    "Matue",
+    "Teto",
+    "Wiu",
+    "Veigh",
+    "Kyan"
+];
+
+// Template Literal/String
+console.log(`Atualmente gosto mais do ${artistas[0]}, e não ouço o ${artistas[2]}.`);
+
+// Concatenação
+console.log("Ouço bastante o "+artistas[3]+", e não ouço o "+artistas[1]+".");
