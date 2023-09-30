f33c801882372e11b7573c9a2d1a7f9a7f2c7b5c
victor <124385408+77bueno@users.noreply.github.com>
2023-09-30 11:58:48 -0300

    .

---
+++ js/07-loops.js
@@ -0,0 +1,23 @@
+console.log("Loops");
+
+/* Principais comandos de repetição
+
+- While     ENQUANTO
+- Do/While  FAÇA ENQUANTO
+- For       PARA
+
+Obs.: Normalmente os comandos são controlados por meio de uma variável controladora. */
+
+// Exemplo 1: While
+/* let i = 1;
+while( i <= 5) {
+    console.log(`Contador vale ${i}`);
+    i++;
+} */
+
+// Exemplo 2: Do/While
+let i = 1;
+do {
+    alert("Olaaa");
+    i++; // Incremento
+} while (i <= 10);
