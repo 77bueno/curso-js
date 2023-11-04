fb5d05d8164a10c809a4a667fbf30e72e9353a22
victor <victorhugomotabueno@gmail.com>
2023-11-03 23:42:03 -0300

    exercicio

---
+++ exercicios/js/exercicio04.js
@@ -0,0 +1,5 @@
+let btAzul = document.getElementById("#btAzul");
+let btLaranja = document.getElementById("#btLaranja");
+let btRosa = document.getElementById("#btRosa");
+
+console.log(btAzul, btLaranja, btRosa);
