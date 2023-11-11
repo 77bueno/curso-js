8d17eb02c43adf7c7451eb4008deb9ed3ea64b3a
victor <124385408+77bueno@users.noreply.github.com>
2023-11-11 08:23:06 -0300

    .

--- exercicios/js/exercicio04.js
+++ exercicios/js/exercicio04.js
@@ -9,6 +9,7 @@ console.log(btAzul, btLaranja, btRosa);
 btAzul.addEventListener("click", functionAzul)
     function functionAzul(event){
         titulo.innerHTML = "<h2>Simboliza criatividade, juventude e alegria.</h2>"
+        titulo.style.color = "blue";
         body.style.transition = "background-color 1s";
         body.style.backgroundColor = "blue"
     }
@@ -16,6 +17,7 @@ btAzul.addEventListener("click", functionAzul)
 btLaranja.addEventListener("click", functionLaranja)
     function functionLaranja(event){
         titulo.innerHTML = "<h2>Simboliza alegria, vitalidade, prosperidade e sucesso.</h2>"
+        titulo.style.color = "orange";
         body.style.transition = "background-color 1s";
         body.style.backgroundColor = "orange"
     }
@@ -23,6 +25,7 @@ btLaranja.addEventListener("click", functionLaranja)
 btRosa.addEventListener("click", functionRosa)
     function functionRosa(event){
         titulo.innerHTML = "<h2>Simboliza romantismo, ternura e ingenuidade.</h2>"
+        titulo.style.color = "pink";
         body.style.transition = "background-color 1s";
         body.style.backgroundColor = "pink"
     }
