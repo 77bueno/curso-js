c2f560efa197d0a369acca9cb41945c0ba7389d2
victor <124385408+77bueno@users.noreply.github.com>
2023-11-18 09:59:47 -0300

    Restante dos arquivos

--- js/12-formulario.js
+++ js/12-formulario.js
@@ -6,7 +6,7 @@ const campoNota1 = formulario.querySelector('#nota1');
 const campoNota2 = formulario.querySelector('#nota2');
 
 // Selecionando a tabela e seus elementos
-const tabela = formulario.querySelector('table');
+const tabela = document.querySelector('table');
 const corpoTabela = tabela.querySelector('tbody');
 
 formulario.addEventListener('submit', function(event){
@@ -18,4 +18,43 @@ formulario.addEventListener('submit', function(event){
         /* Se tem, então removemos o tributo e com isso a tabela volta a aparecer */
         tabela.removeAttribute('hidden');
     }
+
+    /* Capturando os dados digitados */
+    let nome = campoNome.value;
+
+    // Usamos parseFloat para garantir que é tratado como número
+    let nota1 = parseFloat(campoNota1.value);
+    let nota2 = parseFloat(campoNota2.value);
+
+    /* Calculando a média das notas */
+    let media = (nota1 + nota2) / 2;
+
+    /* Verificando a situação do aluno de acordo a média */
+    let situacao; // undefined 
+    if (media >= 7) {
+        situacao = "aprovado(a)";
+    } else {
+        situacao = "reprovado(a)";
+    }
+
+    /* Rotinas para criar uma nova linha e colunas
+    para exibição dos dados no corpo da tabela */
+
+    // 1) Criar Elemento
+    let linha = document.createElement("tr");
+
+    // 2) Montar o conteúdo do elemento
+    linha.innerHTML = 
+            `<td>${nome}</td>
+            <td>${media}</td>
+            <td>${situacao}</td>`;
+
+    // 3) Adicionar na área da página desejada
+    corpoTabela.appendChild(linha);
+
+    // Resetar o formulário
+    formulario.reset();
+
+    // Devolver o foco do cursor para o nome
+    campoNome.focus();
 });
